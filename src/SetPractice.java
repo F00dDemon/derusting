994bb1a85733d90ef75aa99081c0d2f70dc4a4f7
f00d <delossantos.orion@student.greenriver.edu>
2025-10-01 21:38:11 -0700

    that one took me a bit. had to remember static contexts

--- src/SetPractice.java
+++ src/SetPractice.java
@@ -1,7 +1,5 @@
 import java.util.*;
 
-import javax.rmi.ssl.SslRMIClientSocketFactory;
-
 public class SetPractice {
   public static void main(String[] args) {
     // Create a HashSet of Strings and assign it to a variable of type Set
